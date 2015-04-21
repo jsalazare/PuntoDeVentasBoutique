8e98dbcb00468e443f81c116d555d0a6e1512bc1
javiersalazar01 <javi_9315@hotmail.com>
2015-04-20 21:48:16 -0700

    primerCommit
    se agregaron los primeros archivos

---
+++ PuntoDeVentasBoutique/Principal.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace PuntoDeVentasBoutique
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(527, 382);
+            this.Name = "Principal";
+            this.Text = "Super sistema de ventas";
+            this.Load += new System.EventHandler(this.Principal_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ PuntoDeVentasBoutique/SQLConector.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace PruebasSQLServer
+{
+    public class SQLConector
+    {
+        private SqlConnection con;
+        private SqlCommand comand;
+        private static SQLConector util;
+        private SqlDataReader reader;
+        private SqlDataAdapter adapter;
+        private DataSet ds;
+
+
+        public static SQLConector getInstance(){
+            if (util == null)
+            {
+                util = new SQLConector("Data Source=JAVIER-PC\\SQLEXPRESS; Initial Catalog=pruebas; User ID=sa;Password=163955");
+            }
+            return util;
+        }
+
+        private SQLConector(string sConection) {
+            try
+            {
+                con = new SqlConnection(sConection);
+                ds = new DataSet();
+               
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error no posible conectar a BD " + ex.Message);
+            }
+        }
+
+        public SqlConnection getConection() {
+            return con;
+        }
+
+        public DataTable consultar(string sql,string tabla) {
+            try 
+	        {	        
+		        con.Open();
+                adapter  = new SqlDataAdapter(sql,con);
+                if (!ds.Tables.Contains(tabla))
+                {
+                    adapter.Fill(ds, tabla);
+                }
+                con.Close();
+            
+	        }
+	        catch (Exception ex)
+	        {
+		        MessageBox.Show("Error no se puede." + ex.Message + ex.StackTrace);
+	        }
+            return ds.Tables[tabla];
+        }
+
+        public bool modificar(string query) {
+            bool res = true;
+            try
+            {
+                con.Open();
+                comand = con.CreateCommand();
+                comand.CommandText = query;
+                if (comand.ExecuteNonQuery() == -1)
+                {
+                    res = false;
+                }
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Imposible modificacion: " + ex.Message + ex.StackTrace);
+            }
+            return res;
+        }
+
+        //public List<Cliente> getClientes() {
+        //    int res = 0;
+        //    List<Cliente> l = new List<Cliente>();
+        //    comand = con.CreateCommand();
+        //    comand.CommandText = "SELECT * FROM clientes;";
+        //    try
+        //    {
+        //        res = comand.ExecuteNonQuery();
+        //        Cliente c;
+               
+               
+        //        reader = comand.ExecuteReader();
+        //        while (reader.Read())
+        //        {
+        //            c = new Cliente();
+        //            c.id = reader.GetInt32(0);
+        //            c.nombre = reader.GetString(1);
+        //            c.domicilio = reader.GetString(2);
+        //            c.telefono = reader.GetString(3);
+        //            c.email = reader.GetString(4);
+        //            l.Add(c);
+        //        }
+                
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show("Error, Error: " + ex.Message + ex.StackTrace);
+        //    }
+
+        //    return l;
+        //}
+
+        //public bool ingresarCliente(Cliente c) {
+        //    bool res = modificar(""); ;
+            
+
+        //    return res;
+        //}
+
+    }
+}
